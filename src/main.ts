1c212b5e512cb05cc97abfb5b242dc0952602860
venelinkolev <venelinkolev@gmail.com>
2025-08-21 17:17:24 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, withPreloading, withRouterConfig } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, LOCALE_ID } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { registerLocaleData } from '@angular/common';
+
+import localeBG from '@angular/common/locales/bg';
 
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { HTTP_PROVIDERS } from '@core/providers/http.providers';
 import { SelectivePreloadingStrategy } from './app/core/services/selective-preloading-strategy.service';
+
+registerLocaleData(localeBG);
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -24,5 +30,7 @@ bootstrapApplication(AppComponent, {
     provideHttpClient(),
     importProvidersFrom(BrowserAnimationsModule),
     ...HTTP_PROVIDERS,
+    // Регистрация на българския език
+    { provide: LOCALE_ID, useValue: 'bg' }
   ]
 }).catch(err => console.error(err));
