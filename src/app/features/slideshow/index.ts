a0e01b1fc67fee3142242d885ef0f5a93b160bc6
venelinkolev <venelinkolev@gmail.com>
2025-08-25 14:47:20 +0300

    Update index.ts

--- src/app/features/slideshow/index.ts
+++ src/app/features/slideshow/index.ts
@@ -1,4 +1,5 @@
 export * from './slideshow.routes';
 export * from './components/slideshow/slideshow.component';
+export * from './components/slideshow-container/slideshow-container.component';
 export * from './guards';
 // Други експорти ще бъдат добавени по-късно
